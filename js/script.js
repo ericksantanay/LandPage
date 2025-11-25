ae81b017ef2b3943a8fbc35da373530494f79ad2
Erick Silva <donizttierick@gmail.com>
2025-11-25 16:04:07 -0300

    Finalizado a contagen regressiva

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
+// Saídas
+let d = document.getElementById('dia')
+let h = document.getElementById('hora')
+let m = document.getElementById('minutos')
+let s = document.getElementById('segundos')
 
+// Contagem
+let targetDate = new Date("Nov 30, 2025 00:00:00").getTime()
 
+let contador = setInterval(() => {
+    let data = new Date().getTime()
+    let time = targetDate - data
+
+    // Dias
+    let dia = Math.floor(time / (1000 * 60 * 60 * 24))
+    d.innerText = dia
+
+    // Horas
+    let hora = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    h.innerText = hora
+
+    // Minutos
+    let minutos = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
+    m.innerText = minutos
+
+    // Segundos
+    let segundos = Math.floor((time % (1000 * 60)) / 1000)
+    s.innerText = segundos
+
+    // Quando zerar
+    if (time <= 0) {
+        clearInterval(contador)
+    }
+
+}, 1000)
